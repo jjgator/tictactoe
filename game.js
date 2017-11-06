9b7d863239ee573751235bd2f094377ad21918af
Julianne Johnson <tweetypie417@yahoo.com>
2017-11-06 17:19:58 -0600

    player can make moves, working on displaying updated board after each move

--- game.js
+++ game.js
@@ -1,14 +1,18 @@
-//let board = '- - -\n- - -\n- - -';
-let board = [ ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-'] ];
+let board = '- - -\n- - -\n- - -';
+//let board = [ ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-'] ];
 
 let player1 = 'X';
 let player2 = 'O';
 
 let submitMove = (row, column, player) => {
-  board[row][column] = player;
-  return board;
+  board = board.split('\n');
+  let newBoard = [];
+  board.forEach(row => {
+    newBoard.push(row.split(' '));
+  });
+  newBoard[row][column] = player;
+  return newBoard;
 };
-
 submitMove(0, 0, player1);
 
 submitMove(2, 0, player2);
