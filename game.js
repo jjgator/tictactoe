5d273d71e30db98824650c3f83262a92adab5b3c
Julianne Johnson <tweetypie417@yahoo.com>
2017-11-06 16:43:36 -0600

    submit move function added

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-let board = '- - -\n- - -\n- - -';
+//let board = '- - -\n- - -\n- - -';
+let board = [ ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-'] ];
+
+let player1 = 'X';
+let player2 = 'O';
+
+let submitMove = (row, column, player) => {
+  board[row][column] = player;
+  return board;
+};
+
+submitMove(0, 0, player1);
+
+submitMove(2, 0, player2);
